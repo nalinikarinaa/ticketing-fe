d41db156eae6d4893832945fbdf92213e25aec93
Nalini Karina Andhini <nalinikarina613@gmail.com>
2026-01-28 21:53:29 +0700

    add tiket

--- src/views/AddTicket.vue
+++ src/views/AddTicket.vue
@@ -4,14 +4,14 @@
 <div class="h-screen">
     <div class="bg-white rounded-xl shadow p-6 mt-4">
         <h2 class="text-lg font-semibold text-gray-800 text-center">Add Tickets</h2>
-        <form>
+      <form @submit.prevent="storeTicket" class="space-y-2">
         <!-- judul -->
          <div class="mt-6">
             <label class="block text-sm font-medium text-blue-900 mb-1">Judul</label>
             <input
-            
+              v-model="form.judul"
               type="judul"
-              class="form-input w-full border border-ungutext rounded-lg h-10 pl-4 border border-gray-300"
+              class="form-input w-full  rounded-lg h-10 pl-4 border border-gray-300"
               placeholder="judul"
             />
           </div>
@@ -20,6 +20,7 @@
         <div class="mt-6">
             <label class="block text-sm font-medium text-blue-900 mb-1">Kategori</label>
                 <select
+                    v-model="form.kategori"
                     id="kategori"
                     name="kategori"
                     class="mt-1 block w-full rounded-lg border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-ungutua"
@@ -37,6 +38,7 @@
         <div class="mt-6">
             <label class="block text-sm font-medium text-blue-900 mb-1">Prioritas</label>
                 <select
+                    v-model="form.prioritas"
                     id="prioritas"
                     name="prioritas"
                     class="mt-1 block w-full rounded-lg border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-ungutua"
@@ -54,14 +56,14 @@
           <textarea
             placeholder="deskripsi"
             id="deskripsi"
-            v-model="deskripsi"
+            v-model="form.deskripsi"
             rows="4"
             class="block w-full border border-gray-300 rounded-md p-2 text-sm">
           </textarea>
         </div>
 
         <!-- upload file -->
-        <div class="mt-5">
+        <!-- <div class="mt-5">
           <label class="block text-sm font-medium text-blue-900 mb-1">
             Upload Photo
           </label>
@@ -85,13 +87,12 @@
               @change="handleFileUpload"
             >
           </div>
-        </div>
-
-
+        </div> -->
 
          <!-- Tombol Submit -->
           <div class="pt-4">
             <button
+
               type="submit"
               class="bg-green-100 text-green-700 hover hover:bg-gray-200 border-black rounded-md px-6 mt-3"
             >
@@ -109,9 +110,11 @@
 
 <script>
 import SidebarUser from "../components/SidebarUser.vue";
+import axios from 'axios'
+import Swal from 'sweetalert2'
 
 export default {
-  name: "Layout",
+  name: "AddTicket",
   components: {
     SidebarUser,
   },
@@ -125,6 +128,7 @@ export default {
         file: null,
         fileName: ""
       },
+      loading: false,
     };
   },
   methods: {
@@ -137,6 +141,54 @@ export default {
       // nanti di sini bisa kamu fetch ke API Laravel
       alert("Ticket berhasil disubmit!");
     },
+       storeTicket() {
+   if ( !this.form.judul ||
+        !this.form.kategori ||
+        !this.form.prioritas ||
+        !this.form.deskripsi) {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Form belum lengkap',
+      text: 'Harap isi semua field',
+    });
+    return;
+  }
+  const config = {
+    method: 'post',
+    url: import.meta.env.VITE_APP_BACKEND_URL_API + '/addticket',
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+    data: {
+      judul: this.form.judul,
+      kategori: this.form.kategori,
+      prioritas: this.form.prioritas,
+      deskripsi: this.form.deskripsi,
+    },
+  };
+
+  axios(config)
+    .then((response) => {
+      Swal.fire({
+        icon: 'success',
+        title: 'Ticket Berhasil Dibuat!',
+        text: response.data.message || 'Ticket kamu sudah terkirim.',
+      }).then(() => {
+        // redirect setelah sukses
+        this.$router.push('/myticket');
+      });
+    })
+    .catch((error) => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Gagal',
+        text:
+          error.response?.data?.message ||
+          'Terjadi kesalahan saat mengirim ticket',
+      });
+    });
+}
   },
 };
 </script>
